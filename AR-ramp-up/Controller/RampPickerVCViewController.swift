70224c8296d023dda1437f49bcace8bd3377a389
piteli <rushwender@gmail.com>
2020-02-28 06:47:17 +0800

    updated

--- AR-ramp-up/Controller/RampPickerVCViewController.swift
+++ AR-ramp-up/Controller/RampPickerVCViewController.swift
@@ -14,6 +14,7 @@ class RampPickerVCViewController: UIViewController {
     var sceneView : SCNView!
     var size : CGSize!
     
+    
     init(size: CGSize){
         super.init(nibName : nil, bundle:nil)
         self.size = size
@@ -25,19 +26,14 @@ class RampPickerVCViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView (frame : CGRect(x : 0, y : 0, width : size.width, height : size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        preferredContentSize = size
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
