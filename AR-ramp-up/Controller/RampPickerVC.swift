1bcfc168e32111620d024be2585cf5da6b646089
piteli <rushwender@gmail.com>
2020-03-02 19:28:12 +0800

    updated

--- AR-ramp-up/Controller/RampPickerVC.swift
+++ AR-ramp-up/Controller/RampPickerVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SceneKit
 
-class RampPickerVCViewController: UIViewController {
+class RampPickerVC : UIViewController {
     
     var sceneView : SCNView!
     var size : CGSize!
